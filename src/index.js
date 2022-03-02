9e71620565a60d43a3285f980d1962c5bd8afbde
Marcio <marciogomes@alu.ufc.br>
2022-03-02 16:17:58 -0300

    feature: nova branch e .Env

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import './index.css';
 import './app.css'; 
 import SomaNum from './App.js'; 
 
-//Qual a diferença em importar {App} e importar App. 
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <SomaNum /> 
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( 
+    <SomaNum/>,
+    document.getElementById('root')
 );
 
 
